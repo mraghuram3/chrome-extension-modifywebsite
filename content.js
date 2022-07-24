2fc33ebb420d0caf371112ea62c8d69838efc55d
Raghu <raghu@vimals-MacBook-Pro-2.local>
2022-07-24 12:55:44 +0530

    updated extemdion to remove print header from kissflow

--- content.js
+++ content.js
@@ -1,4 +1,9 @@
-var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
-}
+// var images = document.getElementsByTagName('img');
+// for (var i = 0, l = images.length; i < l; i++) {
+//   images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+// }
+var head = document.getElementsByTagName("head")
+// head[0] && head[0].append('<style type="text/css" media="print">.printHeader{display:none;}</style>');
+setTimeout(() => {
+  head[0] && head[0].insertAdjacentHTML("beforeend", `<style>body{background:red}</style><style type="text/css" media="print">.printHeader{display:none;}</style>`);
+},2500);
